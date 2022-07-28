4f702037a8ff35c7d8581b8eefdd2d195366b7e0
Nefeli Metallidou <47782076+nefelimet@users.noreply.github.com>
2022-07-28 18:55:45 +0300

    Update dut.sv

--- up_down_counter/dut.sv
+++ up_down_counter/dut.sv
@@ -5,17 +5,22 @@ input wire rst_, ld_cnt, updn_cnt, count_enb, clk;
 output logic [15:0] data_out;
 
 always_ff@(posedge clk, negedge rst_) begin
+	
+	if(!rst_) begin
+		data_out <= 16'b0;
+	end
+
 	if(!ld_cnt) begin
 		data_out <= data_in;
 	end
 
-	if(count_enb) begin
+	else if(count_enb) begin
 		if(updn_cnt) begin
-			data_out += 1;
+			data_out = data_out + 1;
 		end
 
 		else begin
-			data_out -= 1;
+			data_out = data_out - 1;
 		end
 	end
 end
