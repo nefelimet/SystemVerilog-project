ef991edc14dd2dd88e4af21dac0947489d457290
Nefeli Metallidou <47782076+nefelimet@users.noreply.github.com>
2022-07-28 18:56:07 +0300

    Update dut_property.sv

--- up_down_counter/dut_property.sv
+++ up_down_counter/dut_property.sv
@@ -1,6 +1,7 @@
 module dut_property(pdata_in, prst_, pld_cnt, pupdn_cnt, pcount_enb, pclk, pdata_out);
-input pdata_in, prst_, pld_cnt, pupdn_cnt, pcount_enb, pclk;
-output pdata_out;
+input logic [15:0] pdata_in;
+input wire prst_, pld_cnt, pupdn_cnt, pcount_enb, pclk;
+output logic [15:0] pdata_out;
 
 //Property 1: when reset is asserted, the output of the counter is zero
 property pr1;
@@ -12,8 +13,8 @@ assert property (pr1) else $display("Property 1 FAIL.");
 
 //Property 2: if ld_cnt is deasserted and count_enb is not enabled the counter output does not change. Disabled when rst_ is active (low).
 property pr2;
-	@(posedge pclk) disable iff(prst_)
-	!pld_cnt && !pcount_enb |-> pdata_out == $past(pdata_out);
+	@(posedge pclk) disable iff(!prst_)
+	pld_cnt && !pcount_enb |-> pdata_out == $past(pdata_out);
 endproperty
 
 assert property (pr2) else $display("Property 2 FAIL.");
@@ -22,8 +23,8 @@ assert property (pr2) else $display("Property 2 FAIL.");
 //Property 3: if ld_cnt is deasserted and count_enb is enabled then if updn_cnt is high, the counter increments. Disabled when rst_ is active (low).
 
 property pr3;
-	@(posedge pclk) disable iff(prst_)
-	!pld_cnt && pcount_enb && pupdn_cnt |-> pdata_out == $past(pdata_out) + 1;
+	@(posedge pclk) disable iff(!prst_)
+	pld_cnt && pcount_enb && pupdn_cnt |-> pdata_out == $past(pdata_out) + 1;
 endproperty
 
 assert property (pr3) else $display("Property 3 FAIL.");
@@ -32,8 +33,8 @@ assert property (pr3) else $display("Property 3 FAIL.");
 //Property 4: if ld_cnt is deasserted and count_enb is enabled then if updn_cnt is low, the counter decrements. Disabled when rst_ is active (low).
 
 property pr4;
-	@(posedge pclk) disable iff(prst_)
-	!pld_cnt && pcount_enb && !pupdn_cnt |-> pdata_out == $past(pdata_out) - 1;
+	@(posedge pclk) disable iff(!prst_)
+	pld_cnt && pcount_enb && !pupdn_cnt |-> pdata_out == $past(pdata_out) - 1;
 endproperty
 
 assert property (pr4) else $display("Property 4 FAIL.");
