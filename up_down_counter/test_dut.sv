afb231485c62724e0abf7540e1d514a498fe58d9
Nefeli Metallidou <47782076+nefelimet@users.noreply.github.com>
2022-07-29 17:02:33 +0300

    Update test_dut.sv

--- up_down_counter/test_dut.sv
+++ up_down_counter/test_dut.sv
@@ -9,17 +9,29 @@ dut my_dut(.data_in(sys_data_in), .rst_(sys_rst_), .ld_cnt(sys_ld_cnt), .updn_cn
 bind dut dut_property dutbound(data_in, rst_, ld_cnt, updn_cnt, count_enb, clk, data_out);
 
 always @(posedge sys_clk)
-	$display($stime,,,"data_in = %b, rst=%b, ld_cnt=%b, updn_cnt=%b, count_enb=%b, clk=%b", sys_data_in, sys_rst_, sys_ld_cnt, sys_updn_cnt, sys_count_enb, sys_clk);
+	$display($stime,,,"data_in = %b, rst=%b, ld_cnt=%b, updn_cnt=%b, count_enb=%b, clk=%b, data_out=%b", sys_data_in, sys_rst_, sys_ld_cnt, sys_updn_cnt, sys_count_enb, sys_clk, sys_data_out);
 always #10 sys_clk = !sys_clk;
 
 initial
 begin
 
-	sys_rst_ = 1'b0;
-	@(posedge sys_clk) sys_data_in = 16'b0;
-	@(posedge sys_clk) sys_ld_cnt = 0;
-	@(posedge sys_clk) sys_updn_cnt = 1;
-	@(posedge sys_clk) sys_count_enb = 1;
+	sys_rst_ = 1'b1;
+	sys_data_in = 16'b0;
+	sys_ld_cnt = 0;
+	sys_updn_cnt = 0;
+	sys_count_enb = 0;
+	#60;
+
+	sys_data_in = 7;
+	#60;
+
+	sys_ld_cnt = 1;
+	sys_updn_cnt = 1;
+	sys_count_enb = 1;
+	#60;
+
+	sys_updn_cnt = 0;
+	#60;
 
 end
 
