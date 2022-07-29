bc409f94b30534afc36b72e936bd737730742e55
Nefeli Metallidou <47782076+nefelimet@users.noreply.github.com>
2022-07-29 13:09:10 +0300

    Added comments

--- up_down_counter/dut.sv
+++ up_down_counter/dut.sv
@@ -6,14 +6,18 @@ output logic [15:0] data_out;
 
 always_ff@(posedge clk, negedge rst_) begin
 	
+//If reset is active, data out is zero. Reset is active low.
 	if(!rst_) begin
 		data_out <= 16'b0;
 	end
 
+//If ld_cnt (load counter) is active, load input data to output. ld_cnt is active low.
+//Load counter has a higher priority than count enable.
 	if(!ld_cnt) begin
 		data_out <= data_in;
 	end
 
+//If count enable is active, count up or down, according to updn_cnt's value.
 	else if(count_enb) begin
 		if(updn_cnt) begin
 			data_out = data_out + 1;
