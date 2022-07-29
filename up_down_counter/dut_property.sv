f61fab4953d21db1b3544af01e216c47a3b51838
Nefeli Metallidou <47782076+nefelimet@users.noreply.github.com>
2022-07-29 13:09:43 +0300

    Fixed input/output and printed PASS in assertions too

--- up_down_counter/dut_property.sv
+++ up_down_counter/dut_property.sv
@@ -1,14 +1,15 @@
 module dut_property(pdata_in, prst_, pld_cnt, pupdn_cnt, pcount_enb, pclk, pdata_out);
-input logic [15:0] pdata_in;
+
+input wire [15:0] pdata_in, pdata_out;
 input wire prst_, pld_cnt, pupdn_cnt, pcount_enb, pclk;
-output logic [15:0] pdata_out;
 
 //Property 1: when reset is asserted, the output of the counter is zero
 property pr1;
 	@(posedge pclk) prst_ |-> pdata_out == 0;
 endproperty
 
-assert property (pr1) else $display("Property 1 FAIL.");
+assert property (pr1) $display($stime,,,"\t\t Property 1 PASS.");
+	else $display($stime,,,"\t\t Property 1 FAIL.");
 
 
 //Property 2: if ld_cnt is deasserted and count_enb is not enabled the counter output does not change. Disabled when rst_ is active (low).
@@ -17,7 +18,8 @@ property pr2;
 	pld_cnt && !pcount_enb |-> pdata_out == $past(pdata_out);
 endproperty
 
-assert property (pr2) else $display("Property 2 FAIL.");
+assert property (pr2) $display($stime,,,"\t\t Property 2 PASS.");
+	else $display($stime,,,"\t\t Property 2 FAIL.");
 
 
 //Property 3: if ld_cnt is deasserted and count_enb is enabled then if updn_cnt is high, the counter increments. Disabled when rst_ is active (low).
@@ -27,7 +29,8 @@ property pr3;
 	pld_cnt && pcount_enb && pupdn_cnt |-> pdata_out == $past(pdata_out) + 1;
 endproperty
 
-assert property (pr3) else $display("Property 3 FAIL.");
+assert property (pr3) $display($stime,,,"\t\t Property 3 PASS.");
+	else $display($stime,,,"\t\t Property 3 FAIL.");
 
 
 //Property 4: if ld_cnt is deasserted and count_enb is enabled then if updn_cnt is low, the counter decrements. Disabled when rst_ is active (low).
@@ -37,6 +40,8 @@ property pr4;
 	pld_cnt && pcount_enb && !pupdn_cnt |-> pdata_out == $past(pdata_out) - 1;
 endproperty
 
-assert property (pr4) else $display("Property 4 FAIL.");
+assert property (pr4) $display($stime,,,"\t\t Property 4 PASS.");
+	else $display($stime,,,"\t\t Property 4 FAIL.");
+
 
 endmodule
