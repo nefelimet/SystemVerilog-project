06522d390de351a02cbad960a1f99106672f9489
Nefeli Metallidou <47782076+nefelimet@users.noreply.github.com>
2022-07-29 14:35:48 +0300

    Update test_dut.sv

--- up_down_counter/test_dut.sv
+++ up_down_counter/test_dut.sv
@@ -4,7 +4,7 @@ bit [15:0] sys_data_in;
 bit sys_rst_, sys_ld_cnt, sys_updn_cnt, sys_count_enb, sys_clk;
 bit [15:0] sys_data_out;
 
-dut my_dut(.data_in(sys_data_in), .rst_(sys_rst_), .ld_cnt(sys_ld_cnt), .updn_cnt(sys_updn_cnt), .count_enb(sys_count_enb), .clk(sys_clk));
+dut my_dut(.data_in(sys_data_in), .rst_(sys_rst_), .ld_cnt(sys_ld_cnt), .updn_cnt(sys_updn_cnt), .count_enb(sys_count_enb), .clk(sys_clk), .data_out(sys_data_out));
 
 bind dut dut_property dutbound(data_in, rst_, ld_cnt, updn_cnt, count_enb, clk, data_out);
 
@@ -16,7 +16,10 @@ initial
 begin
 
 	sys_rst_ = 1'b0;
-	//@(posedge sys_clk)
+	@(posedge sys_clk) sys_data_in = 16'b0;
+	@(posedge sys_clk) sys_ld_cnt = 0;
+	@(posedge sys_clk) sys_updn_cnt = 1;
+	@(posedge sys_clk) sys_count_enb = 1;
 
 end
 
