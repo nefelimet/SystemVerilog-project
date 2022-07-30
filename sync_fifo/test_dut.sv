e16c460e64ab29826b90611544a9d55f574455d1
Nefeli Metallidou <47782076+nefelimet@users.noreply.github.com>
2022-07-30 18:57:03 +0300

    Solved simulation warnings

--- sync_fifo/test_dut.sv
+++ sync_fifo/test_dut.sv
@@ -4,9 +4,9 @@ bit [sys_width-1 : 0] sys_fifo_data_in;
 bit sys_rst_, sys_fifo_write, sys_fifo_read, sys_clk, sys_fifo_full, sys_fifo_empty, sys_wr_ptr, sys_rd_ptr, sys_cnt;
 bit [sys_width-1 : 0] sys_fifo_data_out;
 
-dut #(.width(16), .depth(16)) my_dut   (.fifo_data_in(sys_fifo_data_in), .rst_(sys_rst_), .fifo_write(sys_fifo_write), .fifo_read(sys_fifo_read), .clk(sys_clk), .fifo_full(sys_fifo_full), .fifo_empty(sys_fifo_empty));
+dut #(.width(16), .depth(16)) my_dut   (.fifo_data_in(sys_fifo_data_in), .rst_(sys_rst_), .fifo_write(sys_fifo_write), .fifo_read(sys_fifo_read), .clk(sys_clk), .fifo_data_out(sys_fifo_data_out), .fifo_full(sys_fifo_full), .fifo_empty(sys_fifo_empty));
 
-bind dut dut_property #(.depth(16), .width(16)) dutbound(fifo_data_in, rst_, fifo_write, fifo_read, clk, fifo_data_out, fifo_full, fifo_empty);
+bind dut dut_property #(.depth(16), .width(16)) dutbound(fifo_data_in, rst_, fifo_write, fifo_read, clk, fifo_data_out, fifo_full, fifo_empty, dut.cnt, dut.wr_ptr, dut.rd_ptr);
 
 always @(posedge sys_clk)
 	$display($time,,,"");
