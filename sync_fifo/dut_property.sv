71697622dcae596bdb81b2ac439901e931f691f4
Nefeli Metallidou <47782076+nefelimet@users.noreply.github.com>
2022-07-30 12:23:04 +0300

    Update dut_property.sv

--- sync_fifo/dut_property.sv
+++ sync_fifo/dut_property.sv
@@ -1,11 +1,12 @@
-module dut_property(pfifo_data_in, prst_, pfifo_write, pfifo_read, pclk, pfifo_data_out, pfifo_full, pfifo_empty);
+module dut_property #(parameter depth = 16, width = 16) (pfifo_data_in, prst_, pfifo_write, pfifo_read, pclk, pfifo_data_out, pfifo_full, pfifo_empty, pcnt, pwr_ptr, prd_ptr);
 
-input wire [] pfifo_data_in, pfifo_data_out;
-input wire prst_, pfifo_write, pfifo_read, pclk, pfifo_full, pfifo_empty;
+input wire [width-1 : 0] pfifo_data_in, pfifo_data_out;
+input wire prst_, pfifo_write, pfifo_read, pclk, pfifo_full, pfifo_empty, pcnt, pwr_ptr, prd_ptr;
 
 //Property 1:
+//On reset, the read and write pointers are zero, empty and full indicators are ‘1’ and ‘0’, respectively (since the memory is indeed empty), and the counter is zero as well. 
 property pr1;
-	@(posedge pclk) !prst_ |-> (pfifo_write == 0 && pfifo_read == 0 && pfifo_full == 0 && pfifo_empty == 1 && pcnt == 0);
+	@(posedge pclk) !prst_ |-> (pwr_ptr == 0 && prd_ptr == 0 && pfifo_full == 0 && pfifo_empty == 1 && pcnt == 0);
 endproperty
 
 assert property (pr1) $display($stime,,,"\t\t Property 1 PASS.");
@@ -13,8 +14,9 @@ assert property (pr1) $display($stime,,,"\t\t Property 1 PASS.");
 
 
 //Property 2:
-property pr2 disable iff (!prst_);
-	@(posedge pclk) !pcnt |-> pfifo_empty;
+//The fifo empty signal is asserted whenever cnt = 0. Disable this property if the reset is active.
+property pr2;
+	@(posedge pclk) disable iff (!prst_) !pcnt |-> pfifo_empty;
 endproperty
 
 assert property (pr2) $display($stime,,,"\t\t Property 2 PASS.");
@@ -22,8 +24,9 @@ assert property (pr2) $display($stime,,,"\t\t Property 2 PASS.");
 
 
 //Property 3:
-property pr3 disable iff (!prst_);
-	@(posedge pclk) (pcnt >= 16) |-> pfifo_full;
+//The fifo full signal is asserted whenever cnt >= 16. Disable this property if the reset is active.
+property pr3;
+	@(posedge pclk) disable iff (!prst_) (pcnt >= 16) |-> pfifo_full;
 endproperty
 
 assert property (pr3) $display($stime,,,"\t\t Property 3 PASS.");
@@ -31,9 +34,23 @@ assert property (pr3) $display($stime,,,"\t\t Property 3 PASS.");
 
 
 //Property 4:
+//If the FIFO is full and there is write request (but not read), then the write pointer should not change.
 property pr4;
-	@(posedge pclk) (pfifo_full && 
+	@(posedge pclk) (pfifo_full && pfifo_write) |=> (pwr_ptr == $past(pwr_ptr));
 endproperty
+
+assert property (pr4) $display($stime,,,"\t\t Property 4 PASS.");
+	else $display($stime,,,"\t\t Property 4 FAIL.");
+
+
+//Property 5:
+//If the FIFO is empty and there is read request (but not write), then the read pointer should not change. 
+property pr5;
+	@(posedge pclk) (pfifo_empty && pfifo_read) |=> (prd_ptr == $past(prd_ptr));
+endproperty
+
+assert property (pr5) $display($stime,,,"\t\t Property 5 PASS.");
+	else $display($stime,,,"\t\t Property 5 FAIL.");
 
 
 endmodule
