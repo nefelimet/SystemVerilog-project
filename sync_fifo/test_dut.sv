465d4450ea2515fa26463b0fdec3848f36658feb
Nefeli Metallidou <47782076+nefelimet@users.noreply.github.com>
2022-07-30 20:02:01 +0300

    Update test_dut.sv

--- sync_fifo/test_dut.sv
+++ sync_fifo/test_dut.sv
@@ -1,8 +1,9 @@
 module test_dut #(parameter sys_depth = 16, sys_width = 16);
 
 bit [sys_width-1 : 0] sys_fifo_data_in;
-bit sys_rst_, sys_fifo_write, sys_fifo_read, sys_clk, sys_fifo_full, sys_fifo_empty, sys_wr_ptr, sys_rd_ptr, sys_cnt;
+bit sys_rst_, sys_fifo_write, sys_fifo_read, sys_clk, sys_fifo_full, sys_fifo_empty, sys_cnt;
 bit [sys_width-1 : 0] sys_fifo_data_out;
+bit [$clog2(sys_depth) : 0] sys_wr_ptr, sys_rd_ptr;
 
 dut #(.width(16), .depth(16)) my_dut   (.fifo_data_in(sys_fifo_data_in), .rst_(sys_rst_), .fifo_write(sys_fifo_write), .fifo_read(sys_fifo_read), .clk(sys_clk), .fifo_data_out(sys_fifo_data_out), .fifo_full(sys_fifo_full), .fifo_empty(sys_fifo_empty));
 
@@ -13,13 +14,24 @@ always @(posedge sys_clk)
 always #10 sys_clk = !sys_clk;
 
 initial begin
-	sys_rst_ <= 1;
 
+
+	sys_rst_ = 0;
 	sys_fifo_data_in = 7;
 	sys_fifo_write = 1;
 	sys_fifo_read = 0;
 
-	#60;
+	#40;
+
+	sys_rst_ = 1;
+	#40;
+
+	sys_fifo_write = 0;
+	sys_fifo_read = 1;
+	#40;
+
+	sys_fifo_read = 0;
+	#40;
 end
 
 endmodule
