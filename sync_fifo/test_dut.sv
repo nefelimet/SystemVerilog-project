9a12c8d605b035e54ae1fa3a06e45882842406f3
Nefeli Metallidou <47782076+nefelimet@users.noreply.github.com>
2022-07-31 11:17:40 +0300

    Update test_dut.sv

--- sync_fifo/test_dut.sv
+++ sync_fifo/test_dut.sv
@@ -11,7 +11,7 @@ dut #(.width(16), .depth(16)) my_dut   (.fifo_data_in(sys_fifo_data_in), .rst_(s
 bind dut dut_property #(.depth(16), .width(16)) dutbound(fifo_data_in, rst_, fifo_write, fifo_read, clk, fifo_data_out, fifo_full, fifo_empty, dut.cnt, dut.wr_ptr, dut.rd_ptr);
 
 always @(posedge sys_clk)
-	$display($time,,,"wr_ptr=%b, rd_ptr=%b, fifo_full=%b, fifo_empty=%b, mem[0]=%b, mem[1]=%b, mem[2]=%b, mem[3]=%b, mem[4]=%b, mem[5]=%b, mem[6]=%b, mem[7]=%b, mem[8]=%b, mem[9]=%b, mem[10]=%b, mem[11]=%b, mem[12]=%b, mem[13]=%b, mem[14]=%b, mem[15]=%b", my_dut.wr_ptr, my_dut.rd_ptr, my_dut.fifo_full, my_dut.fifo_empty, my_dut.mem[0], my_dut.mem[1], my_dut.mem[2], my_dut.mem[3], my_dut.mem[4], my_dut.mem[5], my_dut.mem[6], my_dut.mem[7], my_dut.mem[8], my_dut.mem[9], my_dut.mem[10], my_dut.mem[11], my_dut.mem[12], my_dut.mem[13], my_dut.mem[14], my_dut.mem[15]);
+	$display($time,,,"wr_ptr=%d, rd_ptr=%d, cnt=%d, fifo_full=%b, fifo_empty=%b, mem[0]=%d, mem[1]=%d, mem[2]=%d, mem[3]=%d, mem[4]=%d, mem[5]=%d, mem[6]=%d, mem[7]=%d, mem[8]=%d, mem[9]=%d, mem[10]=%d, mem[11]=%d, mem[12]=%d, mem[13]=%d, mem[14]=%d, mem[15]=%d", my_dut.wr_ptr, my_dut.rd_ptr, my_dut.cnt, my_dut.fifo_full, my_dut.fifo_empty, my_dut.mem[0], my_dut.mem[1], my_dut.mem[2], my_dut.mem[3], my_dut.mem[4], my_dut.mem[5], my_dut.mem[6], my_dut.mem[7], my_dut.mem[8], my_dut.mem[9], my_dut.mem[10], my_dut.mem[11], my_dut.mem[12], my_dut.mem[13], my_dut.mem[14], my_dut.mem[15]);
 always #10 sys_clk = !sys_clk;
 
 initial begin
@@ -21,7 +21,6 @@ initial begin
 	sys_fifo_data_in = 7;
 	sys_fifo_write = 1;
 	sys_fifo_read = 0;
-
 	#20;
 
 	sys_rst_ = 1;
@@ -70,23 +69,28 @@ initial begin
 	sys_fifo_write = 0;
 	#60;
 
+	sys_fifo_data_in = 103;
+	sys_fifo_write = 1;
+	#60;
+
+	sys_fifo_write = 0;
 	sys_fifo_read = 1;
+	#340
+	
+	sys_fifo_read = 0;
+	#60;
+
+	sys_fifo_read = 1;
+	sys_fifo_data_in = 15;
+	#40;
+
+	sys_fifo_read = 0;
 	#20;
-	#20;
-	#20;
-	#20;
-	#20;
-	#20;
-	#20;
-	#20;
-	#20;
-	#20;
-	#20;
-	#20;
-	#20;
-	#20;
-	#20;
-	#20;
+
+	sys_rst_ = 0;
+	#40;
+
+	
 end
 
 endmodule
